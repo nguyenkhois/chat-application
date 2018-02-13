df5686bb916468ecb6b497e36a74f99e951e9007
Khoi Le <minarek@outlook.com>
2018-02-13 10:19:39 +0100

    Updated the object name and properties follow MDN's guide

--- labs/khoi/object-and-array/scripts/main.js
+++ labs/khoi/object-and-array/scripts/main.js
@@ -1,29 +1,9 @@
-function objProduct(pName, pPrice, pDescription, pImageUrl) {
-    this.productName = pName;
-    this.productPrice = pPrice;
-    this.productDescription = pDescription;
-    this.productImageUrl = pImageUrl;
-}
-function showProductList(pName, pPrice, pDescription, pImageUrl) {
-    let htmlContent = "";
+let objProduct1 = new Product(1,"Apple",10,"Import from France","apple.jpg");
+let objProduct2 = new Product(2,"Banana",20,"Import from Brazil","banana.jpg");
+let objProduct3 = new Product(3,"Lemon",30,"Made in Sweden","lemon.jpg");
 
-    htmlContent += "<p>";
-    htmlContent += pName + "<br>";
-    htmlContent += pPrice + "<br>"
-    htmlContent += pDescription + "<br>";
-    htmlContent += pImageUrl + "<br>";
-    htmlContent += "</p>";
-
-    document.getElementById("dspProducts").innerHTML += htmlContent;
-}
-
-let fruitApple = new objProduct("Apple",10,"Import from France","apple.jpg");
-let fruitBanana = new objProduct("Banana",20,"Import from Brazil","banana.jpg");
-let fruitLemon = new objProduct("Lemon",30,"Made in Sweden","lemon.jpg");
-
-let arrProducts = [fruitApple,fruitBanana,fruitLemon];
+let arrProducts = [objProduct1,objProduct2,objProduct3];
 
 for (let i = 0; i < arrProducts.length; i++){
-    showProductList(arrProducts[i].productName,arrProducts[i].productPrice, arrProducts[i].productDescription,arrProducts[i].productImageUrl);
+    showProductList(arrProducts[i].productId,arrProducts[i].productName,arrProducts[i].productPrice, arrProducts[i].productDescription,arrProducts[i].productImageUrl);
 }
-

508c537beed2826bfd9537aad9eb56035fef5a53
Khoi Le <minarek@outlook.com>
2018-03-03 11:03:45 +0100

    Added processes sign in, sign up and sign out

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,48 +1,48 @@
 $(document).ready(function(){
-    //Clear all error messages
-    $('#txtEmail').on('input',function(){$("#txtEmailNotify").text("");}); //similar with method oninput() in Pure JavaScript
-    $('#txtPassword').on('input',function(){$("#txtPasswordNotify").text("");});
+    auth.onAuthStateChanged(function(user) {
+        if (user) {//User is signed in.
+            goToChat();
+        }else{
+            //Get HTML DOM elements and access several times.
+            let eleEmail = $("#txtEmail");
+            let elePassword = $("#txtPassword");
+            let btnSignIn = $("#btnSignIn");
 
-    //Handle enter key
-    $(document).keydown(function (event) {if (event.keyCode === 13){$("#btnSignIn").click();}});
+            eleEmail.focus();
 
-    //Begin sign in process
-    $("#btnSignIn").click(function () {
-        let uEmail = $("#txtEmail").val();
-        let uPassword = $("#txtPassword").val();
+            //Clear all error messages
+            eleEmail.on('input', function () {$("#txtEmailNotify").text("");}); //similar with method oninput() in Pure JavaScript
+            elePassword.on('input', function () {$("#txtPassword").text("");});
 
-        //Check form and sign in if all input data are good
-        if (checkFormSignIn())
-            signIn(uEmail,uPassword);
+            //Handle enter key (keycode === 13)
+            $(document).keydown(function (event) {if (event.keyCode === 13) {btnSignIn.click();}});
+
+            //Begin sign in process
+            btnSignIn.click(function () {
+                //Check form -true-> Sign in if all input data are good
+                if (checkFormSignIn(eleEmail, elePassword))
+                    signIn(eleEmail.val(), elePassword.val());
+            });
+
+            //Functions
+            function checkFormSignIn(eleEmail, elePassword) {
+                //Form's validation
+                if (eleEmail.val().length < 6 || validateEmailAddress(eleEmail.val()) === false) {
+                    $("#txtEmailNotify").text("Email must be at least 6 characters and have @");
+                    eleEmail.focus();
+                    return false;
+                } else if (elePassword.val().length < 6) {
+                    $("#txtPasswordNotify").text("Password must be at least 6 characters");
+                    elePassword.focus();
+                    return false;
+                } else
+                    return true;
+            }
+            function signIn(email, password) {
+                auth.signInWithEmailAndPassword(email, password)
+                    .then(function () {goToChat();})//Redirect to chat page
+                    .catch(function (error) {writeToLogs(error.code, error.message);});
+            }
+        }
     });
-
-    //Functions
-    function checkFormSignIn() {
-        let email = $("#txtEmail");
-        let password = $("#txtPassword");
-
-        //Form's validation
-        if (email.val().length < 6 || validateEmailAddress(email.val()) === false){
-            $("#txtEmailNotify").text("Email must be at least 5 characters and have @");
-            email.focus();
-            return false;
-        }else if (password.val().length < 6){
-            $("#txtPasswordNotify").text("Password must be at least 6 characters");
-            password.focus();
-            return false;
-        }else
-            return true;
-    }
-    function signIn(email,password) {
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(function () {
-                let user = firebase.auth().currentUser;
-
-                //Get user information
-                writeToLogs(0,"Successfully logged in with userID: " + user.uid);
-                $("#signIn").hide();
-                getUserInfo(user.uid);
-            })
-            .catch(function(error) {writeToLogs(error.code,error.message);});
-    }
 });

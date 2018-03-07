53d0aa30f0fdf37e3efaa551c8c17a363e37cda6
Simon Dellros <simondellros@Simons-MacBook-Air-2.local>
2018-03-07 10:58:29 +0100

    config.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,3 @@
-let config = {
-    apiKey: "AIzaSyBbRmOv-XuKngTAir2avZf7jk1D7jxg9LU",
-    authDomain: "gruppprojekt.firebaseapp.com",
-    databaseURL: "https://gruppprojekt.firebaseio.com",
-    projectId: "gruppprojekt",
-    storageBucket: "gruppprojekt.appspot.com",
-    messagingSenderId: "623943997734"
-};
-firebase.initializeApp(config);
-
 let knapp = document.getElementById("login");
 let paragraf = document.getElementById("paragraf");
 

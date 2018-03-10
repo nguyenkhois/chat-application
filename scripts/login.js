4a1ae0699a2eb243a2077d92b6db9bf1f328b9a9
filipo0on <landedcosine@live.se>
2018-03-10 13:24:35 +0100

    merged all functions

--- scripts/login.js
+++ scripts/login.js
@@ -1,27 +1,63 @@
-<<<<<<< HEAD
-let knapp = document.getElementById("login");
-let paragraf = document.getElementById("paragraf");
-
-
-knapp.addEventListener("click", function(event) {
-    event.preventDefault();
-    let email = document.getElementById("username").value;
-    let password = document.getElementById("pass").value;
-
-
-    auth.signInWithEmailAndPassword(email, password)
-        .then(function(){
-            //let user = auth.currentUser;
-            window.location.href = "index.html"
-            let user = auth.currentUser;
-            console.log(user);
-        })
-        .catch(function (error) {
-            paragraf.innerHTML = "Error: " + error.message;
-        });
-
-});
-=======
 //Created for Simon
 //included in login.html
->>>>>>> 428073b8adc83dad4e6915642922c966d6f78519
+
+$(document).ready(function() {
+    //Get HTML elements
+    let user = auth.currentUser;
+    let knapp = document.getElementById("login");
+    let paragraf = document.getElementById("paragraf");
+
+    //Functions
+
+
+    //MAIN
+
+    auth.onAuthStateChanged(function(user){
+        if (user) {
+            let nodeRef = database.ref("users/" + user.uid);
+            nodeRef.once("value")
+                .then(function(snapshot) {
+
+                    let objUserInfo = {
+                    userId: user.uid,
+                    displayName: snapshot.val().displayName,
+                    phoneNumber: snapshot.val().phoneNumber,
+                    photoUrl: snapshot.val().photoUrl
+                    };
+
+
+                    if (typeof(Storage) !== "undefined")
+                        localStorage.chatappCurrentUserInfo = JSON.stringify(objUserInfo);
+
+                })
+                .catch();
+            console.log("authstatechanged TRUE");
+        }
+        else {
+
+            knapp.addEventListener("click", function(event) {
+                event.preventDefault();
+                let email = document.getElementById("email").value;
+                let password = document.getElementById("pass").value;
+
+                auth.signInWithEmailAndPassword(email, password)
+                    .then(function(user){
+                        if (user) {
+                            let nodeRef = database.ref("users/" + user.uid);
+                            nodeRef.update({isOnline: true})
+                                .then(function () {
+
+                                    window.location.href = "chat.html";
+                                })
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log("signin catch (false)")
+                    });
+
+            });
+            console.log("authstatechanged FALSE");
+        }
+
+    });
+});

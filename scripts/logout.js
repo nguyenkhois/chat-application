b1ae447514866c0d1a842abc2472c33715f669da
Khoi Le <minarek@outlook.com>
2018-03-13 08:48:42 +0100

    Added tools for ChatApp installation and fixed bugs in sign out function

--- scripts/logout.js
+++ scripts/logout.js
@@ -1,7 +1,4 @@
-
-
 $(document).ready(function() {
-
     auth.onAuthStateChanged(function(user){
         if (user) {
             console.log("authstatechanged TRUE");
@@ -11,14 +8,14 @@ $(document).ready(function() {
                     firebase.auth().signOut().then(function() {
                         console.log("signout successful");
 
-                        localStorage.clear();
+                        //localStorage.clear(); It will clear all localStorage on the client
                         localStorage.removeItem("firebase:host:chattapp-b40df.firebaseapp.com");
                         localStorage.removeItem("chatappCurrentUserInfo");
                         localStorage.removeItem("chatappChannelId");
 
                         $(location).attr("href", signInPage);
                     }).catch(function(error) {
-                        console.log("signout failed")
+                        console.log("signout failed" + error.code);
                     });
                 })
                 .catch();
